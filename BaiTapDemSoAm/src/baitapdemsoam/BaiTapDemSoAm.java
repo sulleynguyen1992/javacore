5e338a3eb440c41280a43c67c626d9385ad13e96
sulleynguyen1992 <sulleynguyen@gmail.com>
2017-12-08 00:26:41 +0700

    cap nhat bai tap den so am

--- BaiTapDemSoAm/src/baitapdemsoam/BaiTapDemSoAm.java
+++ BaiTapDemSoAm/src/baitapdemsoam/BaiTapDemSoAm.java
@@ -14,29 +14,66 @@ public class BaiTapDemSoAm {
     /**
      * @param args the command line arguments
      */
-    public static void negativeNumberInString(String s){
+    public static void negativeNumberInString(String s) {
         String newStr = s;
-        String negativeStr= "";
+        String negativeStr = "";
         String[] arr = s.split("");
         int posNegativeSign;
         posNegativeSign = s.indexOf("-");
         for (int i = posNegativeSign + 1; i < newStr.length(); i++) {
-                char a_char =  newStr.charAt(i);
-                if(Character.isDigit(a_char)){
-                    negativeStr += "-" + a_char;
-                }
-                else{
-                   newStr =  newStr.substring(i);
-                    break;
-                }
+            char a_char = newStr.charAt(i);
+            if (Character.isDigit(a_char)) {
+                negativeStr += "-" + a_char;
+            } else {
+                newStr = newStr.substring(i);
+                break;
+            }
         }
         System.out.println("negative Str: " + negativeStr);
         System.out.println("new string: " + newStr);
         System.out.println("pos: " + posNegativeSign);
     }
+
+    public static void negativeNumberCheck(String s) {
+        String newStr = s;
+        //mang de tach chuoi
+        String[] arr = s.split("");
+        String negativeStr = "";
+        String newStrWithAllNegativeNumber = "";
+        int breakPos = 0;
+        for (int i = 0; i < newStr.length(); i++) {
+
+            //tim den sign negative
+            if (arr[i].equalsIgnoreCase("-")) {
+                //tinh tu sign negative dau tien hoac sign tiep theo
+                if (breakPos == 0 || i > breakPos) {
+                    for (int j = i + 1; j < newStr.length(); j++) {
+                        //tao char tai vi tri j
+                        char a_char = newStr.charAt(j);
+                        if (Character.isDigit(a_char)) {
+                            negativeStr += a_char;
+                            breakPos = j;
+                        } else {
+                            break;
+                        }
+
+                    }
+                    if (!negativeStr.equals("")) {
+                        newStrWithAllNegativeNumber += "-" + negativeStr + ",";
+                        negativeStr = "";
+                    }
+                }
+
+            }
+
+        }
+        System.out.println("negative Str: " + newStrWithAllNegativeNumber);
+
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
-        negativeNumberInString("abc-5xyz-12k9pk");
+        negativeNumberCheck("---c-12-13-kc----12----");
     }
-    
+
 }
